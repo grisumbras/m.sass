77f277be204cf2f189513f43940ef453fead4428
Dmitry Arkhipov <grisumbras@gmail.com>
2019-08-09 08:36:25 +0300

    conan packaging

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+from conans import (
+    ConanFile,
+    python_requires,
+)
+
+
+b2 = python_requires("b2-helper/[>=0.5.0]@grisumbras/stable")
+
+
+@b2.build_with_b2
+class MSassConan(ConanFile):
+    name = "m.sass"
+    version = "0.1.0"
+    description = "SASS version of m.css"
+    url = "https://github.com/grisumbras/m.sass"
+    homepage = url
+    license = "MIT"
+
+    exports_sources = (
+        "LICENSE*",
+        "jamroot.jam",
+    )
+
+    def package_info(self):
+        self.info.header_only()
